a856fb332f1633e4773fde8d485a6b974cb3f0c0
luismbm89 <luis.mbm89@gmail.com>
2017-12-10 16:44:48 -0600

    New Message

--- app/src/main/java/com/app/luisbolanos/mailsoft/NewMessage.java
+++ app/src/main/java/com/app/luisbolanos/mailsoft/NewMessage.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -38,11 +39,34 @@ import java.util.concurrent.TimeUnit;
 import static com.microsoft.windowsazure.mobileservices.table.query.QueryOperations.val;
 
 public class NewMessage extends AppCompatActivity {
+    private MobileServiceClient mClient;
+    private MailAdapter mAdapter;
+    private MobileServiceTable<Mail> mToDoTable;
+    private ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+        try {
+            // Create the Mobile Service Client instance, using the provided
 
+            // Mobile Service URL and key
+            mClient = new MobileServiceClient(
+                    "https://isw-1313.azurewebsites.net",
+                    this).withFilter(new NewMessage.ProgressFilter());
+
+            // Extend timeout from default of 10s to 20s
+            mClient.setAndroidHttpClientFactory(new OkHttpClientFactory() {
+                @Override
+                public OkHttpClient createOkHttpClient() {
+                    OkHttpClient client = new OkHttpClient();
+                    client.setReadTimeout(20, TimeUnit.SECONDS);
+                    client.setWriteTimeout(20, TimeUnit.SECONDS);
+                    return client;
+                }
+            });
+        mToDoTable = mClient.getTable(Mail.class);
+        mAdapter = new MailAdapter(this, R.layout.listview);}catch(Exception ex){}
         SharedPreferences prefs =
                 getSharedPreferences("Configuracion", Context.MODE_PRIVATE);
         String email = prefs.getString("email", "");
@@ -50,6 +74,141 @@ public class NewMessage extends AppCompatActivity {
         txtemail.setText(email);
         txtemail.setEnabled(false);
 
+    }
+    private class ProgressFilter implements ServiceFilter {
+
+        @Override
+        public ListenableFuture<ServiceFilterResponse> handleRequest(ServiceFilterRequest request, NextServiceFilterCallback nextServiceFilterCallback) {
+
+            final SettableFuture<ServiceFilterResponse> resultFuture = SettableFuture.create();
+
+
+            runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+            });
+
+            ListenableFuture<ServiceFilterResponse> future = nextServiceFilterCallback.onNext(request);
+
+            Futures.addCallback(future, new FutureCallback<ServiceFilterResponse>() {
+                @Override
+                public void onFailure(Throwable e) {
+                    resultFuture.setException(e);
+                }
+
+                @Override
+                public void onSuccess(ServiceFilterResponse response) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if (mProgressBar != null) mProgressBar.setVisibility(ProgressBar.GONE);
+                        }
+                    });
+
+                    resultFuture.set(response);
+                }
+            });
+
+            return resultFuture;
+        }
+    }
+    public void addItem(View view) {
+        if (mClient == null) {
+            return;
+        }
+
+        // Create a new item
+        final Mail item = new Mail();
+        EditText from =(EditText)findViewById(R.id.txtmsgDe);
+        EditText to =(EditText)findViewById(R.id.txtMsgPara);
+        EditText subject =(EditText)findViewById(R.id.txtmsgAsunto);
+        EditText msg =(EditText)findViewById(R.id.txtMsgBody);
+        item.setFrom(from.getText().toString());
+        item.setTo(to.getText().toString());
+        item.setSubject(subject.getText().toString());
+        item.setMessage(msg.getText().toString());
+
+        // Insert the new item
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>(){
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    final Mail entity = addItemInTable(item);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //if(!entity.isComplete()){
+                            mAdapter.add(entity);
+                            // }
+                        }
+                    });
+                } catch (final Exception e) {
+                    createAndShowDialogFromTask(e, "Error");
+                }
+                return null;
+            }
+        };
+
+        runAsyncTask(task);
+    }
+    public Mail addItemInTable(Mail item) throws ExecutionException, InterruptedException {
+        Mail entity = mToDoTable.insert(item).get();
+        return entity;
+    }
+    private void createAndShowDialogFromTask(final Exception exception, String title) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowDialog(exception, "Error");
+            }
+        });
+    }
+    /**
+     * Run an ASync task on the corresponding executor
+     * @param task
+     * @return
+     */
+    private AsyncTask<Void, Void, Void> runAsyncTask(AsyncTask<Void, Void, Void> task) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            return task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+        } else {
+            return task.execute();
+        }
+    }
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param exception
+     *            The exception to show in the dialog
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(Exception exception, String title) {
+        Throwable ex = exception;
+        if(exception.getCause() != null){
+            ex = exception.getCause();
+        }
+        createAndShowDialog(ex.getMessage(), title);
+    }
+    /**
+     * Creates a dialog and shows it
+     *
+     * @param message
+     *            The dialog message
+     * @param title
+     *            The dialog title
+     */
+    private void createAndShowDialog(final String message, final String title) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(message);
+        builder.setTitle(title);
+        builder.create().show();
     }
 
 
